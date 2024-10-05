cfa539075382a221e21e1669b3b247f97f7de2d8
geilsonfreire <geilsonfreireleite@gmail.com>
2024-10-05 14:05:48 -0300

    Update Node.js version to 18.x in package.json

--- src/config/config.js
+++ src/config/config.js
@@ -1,28 +1,33 @@
 require('dotenv').config(); // Carrega as variáveis de ambiente do .env
 
+// Verificação das variáveis de ambiente para garantir que estão definidas
+if (!process.env.DB_USER || !process.env.DB_PASSWORD || !process.env.DB_DATABASE_PRODUCTION || !process.env.DB_HOST) {
+    throw new Error('Alguma variável de ambiente não foi definida. Verifique o arquivo .env');
+}
+
 module.exports = {
     development: {
         username: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE_DEVELOPMENT,
         host: process.env.DB_HOST,
-        dialect: process.env.DB_DIALECT,
         port: process.env.DB_PORT,
+        dialect: 'postgres',
     },
     test: {
         username: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE_TEST,
         host: process.env.DB_HOST,
-        dialect: process.env.DB_DIALECT,
         port: process.env.DB_PORT,
+        dialect: 'postgres',
     },
     production: {
         username: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE_PRODUCTION,
         host: process.env.DB_HOST,
-        dialect: process.env.DB_DIALECT,
         port: process.env.DB_PORT,
+        dialect: 'postgres',
     },
 };
--- src/config/db.js
+++ src/config/db.js
@@ -7,8 +7,8 @@ const sequelize = new Sequelize(
     process.env.DB_PASSWORD,  // Senha do banco de dados
     {
         host: process.env.DB_HOST,          // Host do banco de dados
-        dialect: process.env.DB_DIALECT,    // Dialeto do banco de dados
         port: process.env.DB_PORT,           // Porta do banco de dados
+        dialect: 'postgres',                // Tipo do banco de dados
         define: {
             timestamps: true,     // Habilita timestamps
             underscored: true,    // Utiliza nomes de colunas com underscore
