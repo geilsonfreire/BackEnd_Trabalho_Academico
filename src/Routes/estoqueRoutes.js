8a9537f4573c172076440ff97ef8845c6bfcb75d
geilsonfreire <geilosnfreireleite@gmail.com>
2024-10-14 18:49:48 -0300

    Refatorar caminho do arquivo de validação de Estoque

--- src/Routes/estoqueRoutes.js
+++ src/Routes/estoqueRoutes.js
@@ -4,13 +4,16 @@ const estoqueController = require('../controllers/estoqueController');
 const authMiddleware = require('../middleware/authMiddleware');
 const roleMiddleware = require('../middleware/roleMiddleware');
 
+// Importar os validadores
+const estoqueValidation = require('../validations/estoqueValidation');
+
 // Ações de leitura (acessíveis publicamente)
 router.get('/', estoqueController.getEstoques); // Acessar todos os estoques
 router.get('/:id', estoqueController.getEstoqueById); // Acessar um estoque por ID
 
 // Ações que modificam (restritas a usuários autenticados e com permissões)
-router.post('/', authMiddleware, roleMiddleware('Administrador'), estoqueController.createEstoque); // Criar novo estoque (apenas Administrador)
-router.put('/:id', authMiddleware, roleMiddleware('Administrador'), estoqueController.updateEstoque); // Atualizar estoque (apenas Administrador)
+router.post('/', authMiddleware, roleMiddleware('Administrador'), estoqueValidation, estoqueController.createEstoque); // Criar novo estoque (apenas Administrador)
+router.put('/:id', authMiddleware, roleMiddleware('Administrador'), estoqueValidation, estoqueController.updateEstoque); // Atualizar estoque (apenas Administrador)
 router.delete('/:id', authMiddleware, roleMiddleware('Administrador'), estoqueController.deleteEstoque); // Excluir estoque (apenas admin)
 
 module.exports = router;
